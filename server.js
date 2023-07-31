affe4c8ff8414cc5d55462d045fdcd0e86a8035d
Anshul Yadav <anshulyadav98@gmail.com>
2023-07-31 07:29:56 +0530

    Fix the error on socket.io

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path'); 
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 app.use(express.static(path.join(__dirname, '/Face-Detection-Javascript-master')));
