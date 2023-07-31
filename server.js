96a605f4e681486ad805a488e0dc6aec5f4c4b16
Khushi Surana <139729599+suranakhushi@users.noreply.github.com>
2023-07-31 06:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,19 @@ const io = require('socket.io')(httpServer);
 app.use(express.static(path.join(__dirname, '/Face-Detection-Javascript-master')));
 io.on('connection', (socket) => {
   console.log('A user connected');
-
-  // Listen for the 'video', 'audio', and 'screen' events from the client
   socket.on('video', (url) => {
     console.log('Received video URL:', url);
-    // Process the video URL on the server as needed
+  
   });
 
   socket.on('audio', (url) => {
     console.log('Received audio URL:', url);
-    // Process the audio URL on the server as needed
+   
   });
 
   socket.on('screen', (url) => {
     console.log('Received screen URL:', url);
-    // Process the screen URL on the server as needed
+    
   });
 
   socket.on('disconnect', () => {
